66d38fe956354a2a5b494f79415ab82dc403eaed
Jawad Maqbool <jawadmaqbool3@gmail.com>
2025-07-24 21:11:00 +0500

    fix: profile fetch function

--- services/MessangerAuthService.js
+++ services/MessangerAuthService.js
@@ -1,11 +1,9 @@
 const MessengerAuthApi = require("../api/Messanger/MessengerAuthApi");
-const MessengerProfileApi = require("../api/Messanger/MessengerProfileApi");
 const SocialAccountRepository = require("../repositories/SocialAccountRepository");
 class MessangerAuthService {
   constructor(user, accessToken) {
     this.user = user;
     this.authApi = new MessengerAuthApi(user, accessToken);
-    this.profileApi = new MessengerProfileApi(user, accessToken);
     this.socialAccountRepository = new SocialAccountRepository();
   }
 
